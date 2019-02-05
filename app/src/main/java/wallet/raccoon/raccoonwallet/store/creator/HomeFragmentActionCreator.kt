a3cbcf93bd92ce1db322fa01e8525946b42cfb29
SasakiYuki <wacorggen@gmail.com>
2019-02-06 00:07:54 +0900

    📝 Add HomeFragmentActionCreator harvest

--- app/src/main/java/wallet/raccoon/raccoonwallet/store/creator/HomeFragmentActionCreator.kt
+++ app/src/main/java/wallet/raccoon/raccoonwallet/store/creator/HomeFragmentActionCreator.kt
@@ -1,2 +1,32 @@
 package wallet.raccoon.raccoonwallet.store.creator
 
+import android.content.Context
+import wallet.raccoon.raccoonwallet.flux.DisposableMapper
+import wallet.raccoon.raccoonwallet.network.Network
+import wallet.raccoon.raccoonwallet.store.type.HomeFragmentActionType
+import wallet.raccoon.raccoonwallet.usecase.HomeFragmentUseCase
+
+class HomeFragmentActionCreator(
+    private val useCase: HomeFragmentUseCase,
+    private val dispatch: (HomeFragmentActionType) -> Unit
+) : DisposableMapper() {
+    suspend fun loadAccountInfo(context: Context) {
+
+    }
+
+    suspend fun loadTransactionList(context: Context) {
+
+    }
+
+    suspend fun loadHarvestInfo(context: Context, address: String) {
+        Network.request(
+            context,
+            useCase.getHarvestInfo(address),
+            {
+                dispatch(HomeFragmentActionType.HarvestInfo(it))
+            }, {
+                it.printStackTrace()
+            }
+        )
+    }
+}
