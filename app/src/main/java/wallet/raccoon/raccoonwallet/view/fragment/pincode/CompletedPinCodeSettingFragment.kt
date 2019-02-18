5996f277f5b6299a6d7d4579886c757b29dbc376
SasakiYuki <wacorggen@gmail.com>
2019-02-18 23:59:27 +0900

    🎞 AddScreen tutorial finish fragment

--- app/src/main/java/wallet/raccoon/raccoonwallet/view/fragment/pincode/CompletedPinCodeSettingFragment.kt
+++ app/src/main/java/wallet/raccoon/raccoonwallet/view/fragment/pincode/CompletedPinCodeSettingFragment.kt
@@ -1,11 +1,25 @@
 package wallet.raccoon.raccoonwallet.view.fragment.pincode
 
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.fragment_completed_pin_code_setting.lessonEndButton
 import wallet.raccoon.raccoonwallet.R
 import wallet.raccoon.raccoonwallet.view.fragment.BaseFragment
+import wallet.raccoon.raccoonwallet.view.fragment.tutorial.TutorialLessonFinishFragment
+import wallet.raccoon.raccoonwallet.view.fragment.tutorial.WalletCreatedType.NEWBIE
 
 class CompletedPinCodeSettingFragment : BaseFragment() {
   override fun layoutRes() = R.layout.fragment_completed_pin_code_setting
+
+  override fun onViewCreated(
+    view: View,
+    savedInstanceState: Bundle?
+  ) {
+    super.onViewCreated(view, savedInstanceState)
+    lessonEndButton.setOnClickListener {
+      replaceFragment(TutorialLessonFinishFragment.newInstance(NEWBIE), true)
+    }
+  }
 
   companion object {
     fun newInstance(): CompletedPinCodeSettingFragment {
