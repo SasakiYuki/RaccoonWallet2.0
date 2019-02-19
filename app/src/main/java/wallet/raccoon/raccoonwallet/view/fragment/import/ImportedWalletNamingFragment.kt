17d37a29be3864ab3484080183159444a1a80e91
SasakiYuki <wacorggen@gmail.com>
2019-02-19 22:15:53 +0900

    🎞 AddScreen ImportFinishedFragment

--- app/src/main/java/wallet/raccoon/raccoonwallet/view/fragment/import/ImportedWalletNamingFragment.kt
+++ app/src/main/java/wallet/raccoon/raccoonwallet/view/fragment/import/ImportedWalletNamingFragment.kt
@@ -1,11 +1,23 @@
 package wallet.raccoon.raccoonwallet.view.fragment.import
 
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.fragment_imported_wallet_naming.submitButton
 import wallet.raccoon.raccoonwallet.R
 import wallet.raccoon.raccoonwallet.view.fragment.BaseFragment
 
 class ImportedWalletNamingFragment : BaseFragment() {
   override fun layoutRes() = R.layout.fragment_imported_wallet_naming
+
+  override fun onViewCreated(
+    view: View,
+    savedInstanceState: Bundle?
+  ) {
+    super.onViewCreated(view, savedInstanceState)
+    submitButton.setOnClickListener {
+      replaceFragment(ImportFinishedFragment.newInstance(), true)
+    }
+  }
 
   companion object {
     fun newInstance(): ImportedWalletNamingFragment {
