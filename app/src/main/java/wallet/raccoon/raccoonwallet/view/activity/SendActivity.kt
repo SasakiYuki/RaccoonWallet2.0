b6c02fd42298e65828a652cc603d51af6e48fa63
SasakiYuki <wacorggen@gmail.com>
2019-02-23 16:04:51 +0900

    🎞 AddScreen SendActivity

---
+++ app/src/main/java/wallet/raccoon/raccoonwallet/view/activity/SendActivity.kt
@@ -0,0 +1,13 @@
+package wallet.raccoon.raccoonwallet.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wallet.raccoon.raccoonwallet.R
+
+class SendActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_send)
+  }
+}
