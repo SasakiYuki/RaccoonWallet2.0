e71d3256d8e8448a0824636151dcebde44ffb665
SasakiYuki <wacorggen@gmail.com>
2019-02-24 21:57:49 +0900

    🎨 Fix Namespace api return

--- app/src/main/java/wallet/raccoon/raccoonwallet/rest/service/NamespaceService.kt
+++ app/src/main/java/wallet/raccoon/raccoonwallet/rest/service/NamespaceService.kt
@@ -1,9 +1,15 @@
 package wallet.raccoon.raccoonwallet.rest.service
 
+import com.ryuta46.nemkotlin.model.MosaicDefinitionMetaDataPair
+import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NamespaceService {
   @GET("/namespace/mosaic/definition/page")
-  fun namespaceMosaicDefinitionPage(@Query("namespace") namespace: String)
+  fun namespaceMosaicDefinitionPage(
+    @Query(
+        "namespace"
+    ) namespace: String
+  ): Deferred<List<MosaicDefinitionMetaDataPair>>
 }
