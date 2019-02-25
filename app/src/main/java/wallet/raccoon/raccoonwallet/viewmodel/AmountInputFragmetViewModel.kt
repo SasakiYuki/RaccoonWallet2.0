6964c3c6a7e829be5da74502d560e12b1098805c
SasakiYuki <wacorggen@gmail.com>
2019-02-25 18:32:37 +0900

    📝 Add AmountInputFragmentViewModel

---
+++ app/src/main/java/wallet/raccoon/raccoonwallet/viewmodel/AmountInputFragmetViewModel.kt
@@ -0,0 +1,36 @@
+package wallet.raccoon.raccoonwallet.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
+import com.ryuta46.nemkotlin.model.Mosaic
+import com.ryuta46.nemkotlin.model.MosaicDefinitionMetaDataPair
+import wallet.raccoon.raccoonwallet.flux.store.AmountInputFragmentStore
+import javax.inject.Inject
+
+class AmountInputFragmetViewModel @Inject constructor(
+  private val context: Context,
+  private val store: AmountInputFragmentStore
+) : BaseViewModel() {
+  val ownedMosaicsData: MutableLiveData<List<Mosaic>> = MutableLiveData()
+  val namespaceMosaicsData: MutableLiveData<List<MosaicDefinitionMetaDataPair>> = MutableLiveData()
+
+  init {
+    addDisposable(store.getter.ownedMosaics
+        .subscribe {
+          ownedMosaicsData.postValue(it)
+        })
+
+    addDisposable(store.getter.namespace
+        .subscribe {
+          namespaceMosaicsData.postValue(it)
+        })
+  }
+
+  suspend fun loadOwnedMosaic(address: String) {
+    store.actionCreator.loadOwnedMosaics(context, address)
+  }
+
+  suspend fun loadNamespaceMosaic(namespace: String) {
+    store.actionCreator.loadNamespaceMosaic(context, namespace)
+  }
+}
