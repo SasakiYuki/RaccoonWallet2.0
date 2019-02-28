dbeed296cff970e741038214bcf36e53e9fb15df
SasakiYuki <wacorggen@gmail.com>
2019-02-28 18:39:46 +0900

    👨🏼‍🔬Rename fragment_amount_input -> fragment_owned_mosaic_select (I miss it !)

--- app/src/main/java/wallet/raccoon/raccoonwallet/view/fragment/send/OwnedMosaicSelectFragment.kt
+++ app/src/main/java/wallet/raccoon/raccoonwallet/view/fragment/send/OwnedMosaicSelectFragment.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProviders
 import dagger.android.support.AndroidSupportInjection
 import wallet.raccoon.raccoonwallet.R
 import wallet.raccoon.raccoonwallet.di.ViewModelFactory
+import wallet.raccoon.raccoonwallet.view.controller.OwnedMosaicSelectListController
 import wallet.raccoon.raccoonwallet.view.fragment.BaseFragment
 import wallet.raccoon.raccoonwallet.viewmodel.OwnedMosaicSelectFragmentViewModel
 import javax.inject.Inject
@@ -14,12 +15,17 @@ class OwnedMosaicSelectFragment : BaseFragment() {
   @Inject
   lateinit var viewModelFactory: ViewModelFactory
   private lateinit var viewModel: OwnedMosaicSelectFragmentViewModel
+  private lateinit var controller : OwnedMosaicSelectListController
 
-  override fun layoutRes() = R.layout.fragment_amount_input
+  override fun layoutRes() = R.layout.fragment_owned_mosaic_select
 
   override fun onAttach(context: Context?) {
     AndroidSupportInjection.inject(this)
     super.onAttach(context)
+
+    controller = OwnedMosaicSelectListController()
+
+
 
     viewModel = ViewModelProviders.of(this, viewModelFactory)
         .get(OwnedMosaicSelectFragmentViewModel::class.java)
