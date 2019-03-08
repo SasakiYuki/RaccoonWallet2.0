f17a6a60f700d58ea0f2b9fe6125fb869a26c944
SasakiYuki <wacorggen@gmail.com>
2019-03-08 16:54:26 +0900

    🎨 DesignFix SendActivity add TabLayout

--- app/src/main/java/wallet/raccoon/raccoonwallet/view/activity/SendActivity.kt
+++ app/src/main/java/wallet/raccoon/raccoonwallet/view/activity/SendActivity.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import dagger.android.AndroidInjection
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
+import wallet.raccoon.raccoonwallet.R
 import wallet.raccoon.raccoonwallet.view.fragment.send.OwnedMosaicSelectFragment
 import javax.inject.Inject
 
@@ -16,12 +17,14 @@ class SendActivity : BaseFragmentActivity(), HasSupportFragmentInjector {
 
   override fun supportFragmentInjector() = fragmentDispatchingAndroidInjector
 
-  override fun setLayout() = SIMPLE_FRAGMENT_ONLY_LAYOUT
+  override fun setLayout() = R.layout.activity_send
   override fun initialFragment() = OwnedMosaicSelectFragment.newInstance()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     AndroidInjection.inject(this)
     super.onCreate(savedInstanceState)
+
+    setToolBarBackButton()
   }
 
   companion object {
