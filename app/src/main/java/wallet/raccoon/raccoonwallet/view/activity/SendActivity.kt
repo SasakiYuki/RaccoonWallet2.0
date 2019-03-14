029d15c5f9847934935734921f040cad9da64a83
SasakiYuki <wacorggen@gmail.com>
2019-03-14 21:25:02 +0900

    🛠 LogicFix TabItemClick Event

--- app/src/main/java/wallet/raccoon/raccoonwallet/view/activity/SendActivity.kt
+++ app/src/main/java/wallet/raccoon/raccoonwallet/view/activity/SendActivity.kt
@@ -4,11 +4,15 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.Tab
 import dagger.android.AndroidInjection
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
+import kotlinx.android.synthetic.main.activity_send.tabLayout
 import wallet.raccoon.raccoonwallet.R
 import wallet.raccoon.raccoonwallet.view.fragment.send.AmountInputFragment
+import wallet.raccoon.raccoonwallet.view.fragment.send.OwnedMosaicSelectFragment
 import javax.inject.Inject
 
 class SendActivity : BaseFragmentActivity(), HasSupportFragmentInjector {
@@ -25,6 +29,26 @@ class SendActivity : BaseFragmentActivity(), HasSupportFragmentInjector {
     super.onCreate(savedInstanceState)
 
     setToolBarBackButton()
+
+    tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+      override fun onTabReselected(p0: Tab?) {
+      }
+
+      override fun onTabUnselected(p0: Tab?) {
+      }
+
+      override fun onTabSelected(p0: Tab?) {
+        p0?.let { tab ->
+          when (tab.position) {
+            0 ->
+              replaceFragment(AmountInputFragment.newInstance(), true)
+            1 ->
+              replaceFragment(OwnedMosaicSelectFragment.newInstance(), true)
+          }
+        }
+      }
+
+    })
   }
 
   companion object {
