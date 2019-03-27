7c6b40de8bd317e9c2d880bd11e03693d7460ef7
SasakiYuki <wacorggen@gmail.com>
2019-03-27 23:40:07 +0900

    🛠 LogicFix Textcount up

--- app/src/main/java/wallet/raccoon/raccoonwallet/view/fragment/send/SendMessageInputFragment.kt
+++ app/src/main/java/wallet/raccoon/raccoonwallet/view/fragment/send/SendMessageInputFragment.kt
@@ -1,11 +1,43 @@
 package wallet.raccoon.raccoonwallet.view.fragment.send
 
 import android.os.Bundle
+import android.view.View
+import com.jakewharton.rxbinding2.widget.RxTextView
+import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.fragment_send_message_input.closeButton
+import kotlinx.android.synthetic.main.fragment_send_message_input.editText
+import kotlinx.android.synthetic.main.fragment_send_message_input.textCountTextView
 import wallet.raccoon.raccoonwallet.R
 import wallet.raccoon.raccoonwallet.view.fragment.BaseFragment
 
 class SendMessageInputFragment : BaseFragment() {
+  private val compositeDisposable = CompositeDisposable()
   override fun layoutRes() = R.layout.fragment_send_message_input
+
+  override fun onViewCreated(
+    view: View,
+    savedInstanceState: Bundle?
+  ) {
+    super.onViewCreated(view, savedInstanceState)
+    closeButton.setOnClickListener {
+      activity?.onBackPressed()
+    }
+    RxTextView.textChanges(editText)
+        .map { item -> item.length }
+        .subscribe { item ->
+          item?.let {
+            textCountTextView.text = getString(R.string.activity_send_mosaic_prefix, it)
+          }
+        }
+        .let {
+          compositeDisposable.add(it)
+        }
+  }
+
+  override fun onDetach() {
+    super.onDetach()
+    compositeDisposable.dispose()
+  }
 
   companion object {
     fun newInstance(): SendMessageInputFragment {
