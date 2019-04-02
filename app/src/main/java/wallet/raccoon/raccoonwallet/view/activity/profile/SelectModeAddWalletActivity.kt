4007094e980e78532e7e506de4c2675ab28c54ed
SasakiYuki <wacorggen@gmail.com>
2019-04-02 11:26:21 +0900

    SelectmodeAddWalletActivityを追加したよ

---
+++ app/src/main/java/wallet/raccoon/raccoonwallet/view/activity/profile/SelectModeAddWalletActivity.kt
@@ -0,0 +1,13 @@
+package wallet.raccoon.raccoonwallet.view.activity.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wallet.raccoon.raccoonwallet.R
+
+class SelectModeAddWalletActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_select_mode_add_wallet)
+  }
+}
